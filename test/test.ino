07da8bfaecf30a7811a34cee3bdc28ba601f256a
TheLoneWanderer4 <aminsennour@gmail.com>
2019-03-23 11:28:46 -0700

    Update, test.ino is now a i2c scanner

--- test/test.ino
+++ test/test.ino
@@ -26,27 +26,27 @@
     // This sketch tests the standard 7-bit addresses
     // Devices with higher bit address might not be seen properly.
     //
-     
+
     #include <Wire.h>
-     
-     
+
+
     void setup()
     {
       Wire.begin();
-     
+
       Serial.begin(9600);
       while (!Serial);             // Leonardo: wait for serial monitor
       Serial.println("\nI2C Scanner");
     }
-     
-     
+
+
     void loop()
     {
       byte error, address;
       int nDevices;
-     
+
       Serial.println("Scanning...");
-     
+
       nDevices = 0;
       for(address = 1; address < 127; address++ )
       {
@@ -55,7 +55,7 @@
         // a device did acknowledge to the address.
         Wire.beginTransmission(address);
         error = Wire.endTransmission();
-     
+
         if (error == 0)
         {
           Serial.print("I2C device found at address 0x");
@@ -63,7 +63,7 @@
             Serial.print("0");
           Serial.print(address,HEX);
           Serial.println("  !");
-     
+
           nDevices++;
         }
         else if (error==4)
@@ -72,12 +72,12 @@
           if (address<16)
             Serial.print("0");
           Serial.println(address,HEX);
-        }    
+        }
       }
       if (nDevices == 0)
         Serial.println("No I2C devices found\n");
       else
         Serial.println("done\n");
-     
+
       delay(5000);           // wait 5 seconds for next scan
     }
