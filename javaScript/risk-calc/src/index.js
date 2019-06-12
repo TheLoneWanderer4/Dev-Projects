e1145f312624efed1be7c022a6649774e3dc8d89
TheLoneWanderer4 <aminsennour@gmail.com>
2019-06-12 13:45:59 -0600

    Update, created the react-project for my risk app

---
+++ javaScript/risk-calc/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class Game extends React.Component {
+  render() {}
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
