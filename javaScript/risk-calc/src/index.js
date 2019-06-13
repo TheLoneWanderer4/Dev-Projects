6b8fee1ac4f9b2689d7c5b1d22517e282ac7d22c
TheLoneWanderer4 <aminsennour@gmail.com>
2019-06-12 19:44:03 -0600

    Update, clobbered together the starting of the risk app

--- javaScript/risk-calc/src/index.js
+++ javaScript/risk-calc/src/index.js
@@ -2,8 +2,66 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Game extends React.Component {
-  render() {}
+class FormComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { Attack: 0, Defense: 0, canChange: true };
+
+    this.handleChangeAttack = this.handleChangeAttack.bind(this);
+    this.handleChangeDefense = this.handleChangeDefense.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeAttack(event) {
+    if (this.state.canChange) {
+      this.setState({ Attack: parseInt(event.target.value) });
+    }
+  }
+
+  handleChangeDefense(event) {
+    if (this.state.canChange) {
+      this.setState({ Defense: parseInt(event.target.value) });
+    }
+  }
+
+  logic() {
+    this.setState({
+      Attack: this.state.Attack - 1,
+      Defense: this.state.Defense - 1
+    });
+  }
+
+  handleSubmit(event) {
+    alert(this.logic());
+    this.setState({
+      canChange: false
+    });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Attack:
+          <input
+            type="number"
+            value={this.state.Attack}
+            onChange={this.handleChangeAttack}
+          />
+        </label>
+        <label>
+          Defense:
+          <input
+            type="number"
+            value={this.state.Defense}
+            onChange={this.handleChangeDefense}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<FormComponent />, document.getElementById("root"));
