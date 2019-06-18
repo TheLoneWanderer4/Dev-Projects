bac8d80edc8dcae18cbfcbe71a17cf096114e80e
TheLoneWanderer4 <aminsennour@gmail.com>
2019-06-18 14:22:45 -0500

    Update, more or less finished the counter app

--- javaScript/counter/src/App.js
+++ javaScript/counter/src/App.js
@@ -9,18 +9,51 @@ class App extends Component {
     };
   }
 
-  handleCreate = () => {
-    this.setState({ items: this.state.items.concat(["new"]) });
+  handleCreate = event => {
+    event.preventDefault();
+
+    let item = this.input.value;
+    let items = this.state.items;
+
+    if (items.indexOf(item) != -1) {
+      alert(`${item} Already in list`);
+      return;
+    }
+
+    items.push(this.input.value);
+    this.setState({ items: items });
+  };
+
+  handleRemove = item => {
+    let hold = this.state.items;
+    hold.splice(this.state.items.indexOf(item), 1);
+    this.setState({
+      items: hold
+    });
   };
 
   render() {
     return (
-      <div>
+      <div className="m-2">
         <h1> Food list </h1>
+        <form onSubmit={this.handleCreate}>
+          <input ref={input => (this.input = input)} type="text" />
+          <input className="btn btn-sm m-1 btn-warning" type="submit" />
+        </form>
         {this.state.items.map(item => {
-          return <Counter value={item} />;
+          return (
+            <div className="m-2">
+              <Counter key={item} value={item} />
+              <button
+                className="btn btn-sm m-1 btn-danger"
+                onClick={() => this.handleRemove(item)}
+              >
+                {" "}
+                Remove{" "}
+              </button>
+            </div>
+          );
         })}
-        <button onClick={this.handleCreate}> Create new Item </button>
       </div>
     );
   }
