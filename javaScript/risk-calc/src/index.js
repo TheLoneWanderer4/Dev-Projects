e86e33196c107c6f38b09439dbeb83dabbe54713
TheLoneWanderer4 <aminsennour@gmail.com>
2019-06-23 10:09:51 -0500

    Update

--- javaScript/risk-calc/src/index.js
+++ javaScript/risk-calc/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "css/index.css";
 import { HashRouter, Route, Link } from "react-router-dom";
 
 var seedrandom = require("seedrandom");
