d248c173522c6f16d8e8e04797ed3f3d2a59582a
TheLoneWanderer4 <aminsennour@gmail.com>
2019-06-23 10:11:41 -0500

    Update, re-configured CSS routing

--- javaScript/risk-calc/src/index.js
+++ javaScript/risk-calc/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "css/index.css";
+import "./css/index.css";
 import { HashRouter, Route, Link } from "react-router-dom";
 
 var seedrandom = require("seedrandom");
