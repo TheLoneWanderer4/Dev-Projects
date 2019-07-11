0699994c7cdc2362e6ba02c286a3c93f5bf2cd48
TheLoneWanderer4 <aminsennour@gmail.com>
2019-07-11 18:22:59 -0500

    Update, finsihed the randomq uote machine!!!!!

--- FCC Front-End Projects/random-quote-machine/src/index.js
+++ FCC Front-End Projects/random-quote-machine/src/index.js
@@ -4,15 +4,38 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 const Quote = ({ quote }) => (
-  <div className="card-title" id="text">
+  <div className="card-title m-2" id="text">
     {quote}
   </div>
 );
 
+const Author = ({ author }) => (
+  <div id="author" className="mt-4">
+    <div>
+      {"- "}
+      {author}
+    </div>
+  </div>
+);
+
 const NewQuoteButton = ({ onClick }) => (
-  <button className="btn btn-primary" onClick={onClick}>
-    {" "}
-    New Quote{" "}
+  <button id="new-quote" className="btn btn-secondary" onClick={onClick}>
+    New Quote
+  </button>
+);
+
+const TwitterButton = ({ quote, author }) => (
+  <button className="btn btn-secondary">
+    <a
+      id="tweet-quote"
+      className=""
+      href={
+        "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+        encodeURIComponent('"' + quote + '" ' + author)
+      }
+    >
+      Tweet
+    </a>
   </button>
 );
 
@@ -20,15 +43,21 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes: ["u", "q", "g", "f"],
-      colors: [],
-      currentQuote: "test",
-      currentColor: "#fff"
+      quotes: [],
+      currentQuote: { quote: "", author: "" }
     };
   }
 
   componentDidMount() {
-    //make ajax call
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        const quotes = res.quotes;
+        this.setState({ quotes, currentQuote: quotes[0] });
+      });
   }
 
   handleNewQuote = () => {
@@ -41,10 +70,21 @@ class App extends Component {
 
   render() {
     return (
-      <div id="container">
-        <div className="card content p-2 mt-2">
-          <Quote quote={this.state.currentQuote} />
-          <NewQuoteButton onClick={this.handleNewQuote} />
+      <div className="container">
+        <div className="card shadow content p-4" id="quote-box">
+          <Quote id="text" quote={this.state.currentQuote.quote} />
+          <div className="buttons mt-2">
+            <div className="btn-group">
+              <TwitterButton
+                quote={this.state.currentQuote.quote}
+                author={this.state.currentQuote.author}
+              />
+            </div>
+            <Author id="author" author={this.state.currentQuote.author} />
+            <div className="btn-group">
+              <NewQuoteButton onClick={this.handleNewQuote} />
+            </div>
+          </div>
         </div>
       </div>
     );
