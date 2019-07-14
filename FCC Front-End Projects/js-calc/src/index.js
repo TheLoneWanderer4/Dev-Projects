c66c5729b98d3c23f1a0be44e67f13b5c7a5a43e
TheLoneWanderer4 <aminsennour@gmail.com>
2019-07-14 12:31:01 -0500

    Update, started the calculator, but need to change how I'm conceptualizing it.

---
+++ FCC Front-End Projects/js-calc/src/index.js
@@ -0,0 +1,103 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+const buttons = [
+  { value: "*", id: "multiply", className: "" },
+  { value: "/", id: "divide", className: "" },
+  { value: "1", id: "one", className: "" },
+  { value: "2", id: "two", className: "" },
+  { value: "3", id: "three", className: "" },
+  { value: "4", id: "four", className: "" },
+  { value: "5", id: "five", className: "" },
+  { value: "6", id: "six", className: "" },
+  { value: "7", id: "seven", className: "" },
+  { value: "8", id: "eight", className: "" },
+  { value: "9", id: "nine", className: "" },
+  { value: "0", id: "zero", className: "jumbo" },
+  { value: "+", id: "add", className: "" },
+  { value: "-", id: "subtract", className: "" },
+  { value: ".", id: "decimal", className: "" }
+];
+
+const isOperator = /[*/+-]/,
+  endsWithOperator = /[*+-/]$/,
+  leadingZero = /\b0+/g,
+  constainsDecimal = /./g;
+
+const reset = { input: "0", formula: "0" };
+
+const Button = ({ onClick, value, className }) => (
+  <button className={className} onClick={() => onClick(value)}>
+    {" "}
+    {value}{" "}
+  </button>
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = reset;
+  }
+
+  handleEqual = () => {
+    let formula = this.state.formula;
+
+    console.log(formula);
+
+    if (formula.match(endsWithOperator)) {
+      formula = "Error: Cannont end in Operator";
+    } else {
+      formula = eval(this.state.formula).toString();
+    }
+
+    this.setState({
+      input: "0",
+      formula: formula
+    });
+  };
+
+  handleClear = () => {
+    this.setState(reset);
+  };
+
+  handleButton = value => {
+    let formula = this.state.formula.replace(leadingZero, "");
+
+    if (value.match(isOperator) && formula.match(endsWithOperator)) {
+      formula = formula.substring(0, formula.length - 1);
+    }
+
+    this.setState({ input: value, formula: formula + value });
+  };
+
+  render() {
+    return (
+      <div className="calculator">
+        <div id="display" className="outputScreen">
+          {" "}
+          {this.state.formula}{" "}
+        </div>
+        <div className="formulaScreen">{this.state.input}</div>
+        <div>
+          <button className="jumbo" onClick={this.handleClear}>
+            AC
+          </button>
+          {buttons.map(button => (
+            <Button
+              id={button.id}
+              key={button.id}
+              onClick={this.handleButton}
+              value={button.value}
+              className={button.className}
+            />
+          ))}
+          <button onClick={this.handleEqual}>=</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
