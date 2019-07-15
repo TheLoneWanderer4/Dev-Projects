b7a8a120b8dee889bf83ae4035328df693ffc234
TheLoneWanderer4 <aminsennour@gmail.com>
2019-07-15 13:06:43 -0500

    Started pomodoro clock!

---
+++ FCC Front-End Projects/pomodoro-clock/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
