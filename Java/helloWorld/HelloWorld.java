322ae69f6300dd2c890586bcc9c0d0d86d2003a1
TheLoneWanderer4 <aminsennour@gmail.com>
2019-08-31 16:19:34 -0700

    Update

--- Java/helloWorld/HelloWorld.java
+++ Java/helloWorld/HelloWorld.java
@@ -1,9 +1,13 @@
-package helloWorld;
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
+		int x = 1;
+		for(int i = 0; i < 100; i++){
+			System.out.println(i * x);
+		}
+
 	}
 
 }
